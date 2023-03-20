e889bb161e4a22a22a42c8c6d8f01449353e822a
Mei-2513 <j-uliana9@hotmail.com>
2023-03-19 23:27:12 -0500

    correcion de errores

--- src/presenter/ArchivoPrincipal.java
+++ src/presenter/ArchivoPrincipal.java
@@ -2,10 +2,12 @@ package presenter;
 
 import java.io.IOException;
 
+import view.ArchivoVista;
+
 public class ArchivoPrincipal {
 
 	public static void main(String[] args) {
-	    ArchivoVista vista = new ArchivoVista();
+		ArchivoVista vista = new ArchivoVista();
 	    try {
 	        vista.solicitarRutas();
 	    } catch (IOException e) {
